eb879c21995402b03afbdf9a050243251191dc78
flixoflax <flixoflax@gmx.de>
2022-10-05 11:54:38 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["General Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["Satoshi", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
