a9eaedf8e420a28ce59e0a721adc7cd69e690ff9
flixoflax <flixoflax@gmx.de>
2023-04-03 13:34:59 +0200

    Fix Build Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import rehypeTitleFigure from "rehype-title-figure";
 
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   markdown: {
     rehypePlugins: [rehypeTitleFigure],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   }
 });
